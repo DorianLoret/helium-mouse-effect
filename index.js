133ac43bc803864467eb9eb085d8a7d04bff29fa
Dorian Loret <dorian.loret20@gmail.com>
2022-06-01 16:59:02 +0200

    achieved

--- index.js
+++ index.js
@@ -5,3 +5,20 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const mousemove1 = document.querySelector(".mousemove1");
+const mousemove2 = document.querySelector(".mousemove2");
+const mousemove3 = document.querySelector(".mousemove3");
+
+window.addEventListener("mousemove", (e) => {
+    mousemove1.style.top = e.pageY + "px";
+    mousemove1.style.left = e.pageX + "px";
+});
+window.addEventListener("mousemove", (e) => {
+    mousemove2.style.top = e.pageY + "px";
+    mousemove2.style.left = e.pageX + "px";
+});
+window.addEventListener("mousemove", (e) => {
+    mousemove3.style.top = e.pageY + "px";
+    mousemove3.style.left = e.pageX + "px";
+});
